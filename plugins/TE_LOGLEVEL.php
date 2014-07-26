11d8942f7059d4fb11ca81e935a39c5fe58f858e
Obexer Christoph <cobexer@gmail.com>
2014-07-26 22:56:55 +0200

    TE_LOGLEVEL: slight change for better coverage

--- plugins/TE_LOGLEVEL.php
+++ plugins/TE_LOGLEVEL.php
@@ -13,13 +13,14 @@
  */
 
 function TE_PLUGIN_TE_LOGLEVEL(array $ctx, array $match) {
+	$result = false;
 	switch($match[1]) {
-		case 'DEBUG':  TemplateEngine :: setMode(TEMode :: debug);  return '';
-		case 'WARNING':TemplateEngine :: setMode(TEMode :: warning);return '';
-		case 'ERROR':  TemplateEngine :: setMode(TEMode :: error);  return '';
-		case 'NONE':   TemplateEngine :: setMode(TEMode :: none);   return '';
+		case 'DEBUG':  TemplateEngine :: setMode(TEMode :: debug);  $result = ''; break;
+		case 'WARNING':TemplateEngine :: setMode(TEMode :: warning);$result = ''; break;
+		case 'ERROR':  TemplateEngine :: setMode(TEMode :: error);  $result = ''; break;
+		case 'NONE':   TemplateEngine :: setMode(TEMode :: none);   $result = ''; break;
 	}
-	return false;
+	return $result;
 }
 
 TemplateEngine :: registerPlugin('TE_LOGLEVEL', '/\{LOGLEVEL=(DEBUG|WARNING|ERROR|NONE)\}/', 'TE_PLUGIN_TE_LOGLEVEL');
